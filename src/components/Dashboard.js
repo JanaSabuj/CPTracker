1dc849acea845b97688953a243db6e54e914cdfa
Sabuj Jana <sabujlovescoding@gmail.com>
2020-04-14 18:40:38 +0530

    connect store to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,28 @@
-import React from "react";
-import connect from "react-redux";
+import React, { Fragment } from "react";
+import { connect } from "react-redux";
 
-const Dashboard = () => {
-  return <div> Hello</div>;
+const Dashboard = props => {
+  const { sites } = props;
+
+  const introCards = (
+    <Fragment>
+      {sites.length &&
+        sites.map(el => <div className="white-text"> {el.name} </div>)}
+    </Fragment>
+  );
+
+  return (
+    <div>
+      {console.log(sites)}
+      {introCards}
+    </div>
+  );
 };
 
-// const mapStateToProps = () => {
-//   sites:
-// }
+const mapStateToProps = state => {
+  return {
+    sites: state.dashboardReducer.sites
+  };
+};
 
-export default Dashboard;
+export default connect(mapStateToProps)(Dashboard);
