f5c798a94f9c977bf9aeeacc6c38e9e6da6da6f5
Sabuj Jana <sabujlovescoding@gmail.com>
2020-04-14 17:24:13 +0530

    Side Navbar set up.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import "./styles.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import M from "materialize-css/dist/js/materialize.min.js";
 
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <Switch>
-          <Route exact path="/about" component={About} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
+class App extends Component {
+  componentDidMount() {
+    const sidenav = document.querySelector("#slide-out");
+    M.Sidenav.init(sidenav, {});
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
+
+export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
@@ -7,24 +7,73 @@ const Navbar = () => {
   };
 
   return (
-    <nav>
-      <div className="nav-wrapper blue-grey darken-4">
-        <NavLink
-          to="/"
-          className="brand-logo center active"
-          style={styleNavbar}
-        >
-          CPTracker
-        </NavLink>
-        <ul id="nav-mobile" className="right">
-          <li>
-            <NavLink to="/about" style={styleNavbar}>
-              About
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <Fragment>
+      <nav>
+        <div className="nav-wrapper blue-grey darken-4">
+          <a
+            href="/#"
+            data-target="slide-out"
+            className="sidenav-trigger show-on-large"
+          >
+            <i className="material-icons">menu</i>
+          </a>
+          <NavLink
+            to="/"
+            className="brand-logo center active"
+            style={styleNavbar}
+          >
+            CPTracker
+          </NavLink>
+          <ul id="nav-mobile" className="right">
+            <li>
+              <NavLink to="/about" style={styleNavbar}>
+                About
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <ul id="slide-out" className="sidenav sidenav-close">
+        <li>
+          <div className="user-view">
+            <div className="background">
+              <img src="../../img/hack.jpg" alt="coder" />
+            </div>
+
+            <img className="circle" src="../../img/dali.png" alt="coder" />
+
+            <a href="/#">
+              <span className="white-text name">Coder</span>
+            </a>
+            <a href="#email">
+              <span className="white-text email">Talk is Cheap.</span>
+            </a>
+          </div>
+        </li>
+        <li>
+          <NavLink to="/about" className="waves-effect">
+            <span> Codeforces </span>
+            <i className="small material-icons">send</i>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/#" className="waves-effect">
+            <span> Codechef </span>
+            <i className="small material-icons">send</i>
+          </NavLink>
+        </li>
+        <li>
+          <div className="divider" />
+        </li>
+        <li>
+          <a href="/#" className="subheader">
+            <i className="small material-icons red-text">favorite</i> Built by
+            Sabuj Jana.
+          </a>
+        </li>
+      </ul>
+    </Fragment>
   );
 };
 
