9a16dff3989f80329213ec31f6ccd143dda2cb9e
Sabuj Jana <sabujlovescoding@gmail.com>
2020-04-14 12:53:28 +0530

    added About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,28 +1,56 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const About = () => {
   const styleAbout = {
-    backgroundColor: "red"
+    fontSize: "20px"
+  };
+
+  const styleName = {
+    fontFamily: "Megrim",
+    fontSize: "30px"
   };
   return (
-    <div class="row" style={styleAbout}>
-      <div class="col s12 m6">
-        <div class="card blue-grey darken-1">
-          <div class="card-content white-text">
-            <span class="card-title">Card Title</span>
-            <p>
-              I am a very simple card. I am good at containing small bits of
-              information. I am convenient because I require little markup to
-              use effectively.
-            </p>
-          </div>
-          <div class="card-action">
-            <a href="/#">This is a link</a>
-            <a href="/#">This is a link</a>
+    <Fragment>
+      <div className="row" style={{ height: "75px" }} />
+      <div className="row" style={styleAbout}>
+        <div className="col s0 m3"> </div>
+        <div className="col s12 m6">
+          <div className="card grey darken-1">
+            <div className="card-content white-text">
+              <span className="card-title">
+                {" "}
+                <i className="material-icons red-text">favorite</i>
+              </span>
+              <p>App Version 1.0.0</p>
+              <p>
+                Built by <span style={styleName}>Sabuj Jana</span>{" "}
+                <i className="tiny material-icons">copyright</i> 2020
+              </p>
+            </div>
+            <div className="card-action">
+              <form
+                action="https://github.com/JanaSabuj/random-advice"
+                target="_blank"
+              >
+                <button
+                  className="btn waves-effect waves-light brown darken-1"
+                  style={{ marginRight: "10px" }}
+                >
+                  Github
+                  <i className="small material-icons left">code</i>
+                </button>
+
+                <button className="btn waves-effect waves-light brown darken-1">
+                  Website
+                  <i className="small material-icons right">fingerprint</i>
+                </button>
+              </form>
+            </div>
           </div>
         </div>
+        <div className="col s0 m3"> </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
