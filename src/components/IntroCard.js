c665528e783c1c3270790ca2a3757015a5b26722
Sabuj Jana <sabujlovescoding@gmail.com>
2020-04-14 22:51:52 +0530

    IntroCard fixes 2

--- src/components/IntroCard.js
+++ src/components/IntroCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const IntroCard = props => {
   const sites = props.info;
@@ -8,37 +9,63 @@ const IntroCard = props => {
   ];
   const styleName = { fontFamily: "Bungee", fontSize: "20px" };
   return (
-    <div className="row">
-      <div className="col s12 m1" />
-      <div className="col s12 m5">
-        <div className="card-panel grey lighten-5 z-depth-1">
-          <div className="row valign-wrapper">
-            <div className="col s4">
-              <img src={imgPath[0]} alt="" className="circle responsive-img" />
-            </div>
-            <div className="col s8">
-              <span style={styleName}>{sites.name[0]}</span>
-              <div className="divider"> </div>
+    <>
+      <div className="row">
+        <div className="col s12 m1" />
+        <div className="col s12 m5">
+          <div className="card-panel brown lighten-5 z-depth-1">
+            <div className="row valign-wrapper">
+              <div className="col s4">
+                <img
+                  src={imgPath[0]}
+                  alt=""
+                  className="circle responsive-img"
+                />
+              </div>
+              <div className="col s8">
+                <div className="row">
+                  <span style={styleName}>{sites.name[0]}</span>{" "}
+                </div>
+                <Link
+                  className="waves-effect waves-light btn grey darken-3"
+                  to="/about"
+                >
+                  Contests
+                  <i className="material-icons right">code</i>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      {/*  */}
-      <div className="col s12 m5">
-        <div className="card-panel grey lighten-5 z-depth-1">
-          <div className="row valign-wrapper">
-            <div className="col s4">
-              <img src={imgPath[1]} alt="" className="circle responsive-img" />
-            </div>
-            <div className="col s8">
-              <span style={styleName}>{sites.name[1]}</span>
-              <div className="divider"> </div>
+        {/*  */}
+        <div className="col s12 m5">
+          <div className="card-panel grey lighten-5 z-depth-1">
+            <div className="row valign-wrapper">
+              <div className="col s4">
+                <img
+                  src={imgPath[1]}
+                  alt=""
+                  className="circle responsive-img"
+                />
+              </div>
+              <div className="col s8">
+                <div className="row">
+                  <span style={styleName}>{sites.name[1]}</span>{" "}
+                </div>
+                <Link
+                  className="waves-effect waves-light btn grey darken-3"
+                  to="/about"
+                >
+                  Contests
+                  <i className="material-icons right">code</i>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
+        <div className="col s12 m1" />
       </div>
-      <div className="col s12 m1" />
-    </div>
+    </>
   );
 };
 
