5e886630b31b3cdc078f7cd0bdd41546421baabb
Sabuj Jana <sabujlovescoding@gmail.com>
2020-04-14 22:27:28 +0530

    images added.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment } from "react";
 import { connect } from "react-redux";
 import IntroCard from "./IntroCard";
 
--- src/redux/dashboard/dashboardReducer.js
+++ src/redux/dashboard/dashboardReducer.js
@@ -2,9 +2,9 @@ const initialState = {
   sites: [
     { id: 1, name: ["Codeforces", "Codechef"] },
     { id: 2, name: ["Atcoder", "Topcoder"] },
-    { id: 3, name: ["HackerEarth", "HackerRank"] },
-    { id: 4, name: ["LeetCode", "FB Contests"] },
-    { id: 5, name: ["Google Contests", "Other Sites"] }
+    { id: 3, name: ["Hackerearth", "Hackerrank"] },
+    { id: 4, name: ["Leetcode", "Facebook"] },
+    { id: 5, name: ["Google", "Others"] }
   ]
 };
 
